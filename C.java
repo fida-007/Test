9fdbceb242c591c361afc8e6a851b888b90195cb
fida-007 <125455263+fida-007@users.noreply.github.com>
2023-10-03 20:20:29 +0530

    Commit for testing

---
+++ C.java
@@ -0,0 +1,16 @@
+package com.obsqura.training.programs;
+
+import com.obsqura.training.loops.A;
+import com.obsqura.training.loops.B;
+import com.obsqura.training.loops.D;
+
+
+public class C {
+   
+	public static void main(String[] args) {
+		 
+		  A obj1 = new A();
+		  obj1.display();
+		  
+	}
+}
